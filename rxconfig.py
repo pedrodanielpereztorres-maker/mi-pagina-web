48cf128049c44d5372b9bd53ff9b07373c9c4cac
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 18:52:33 -0400

    Fix: Corregir api_url para la conexión WebSocket en Render

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import os
 config = rx.Config(
     app_name="hola",
     db_url=os.environ.get("DATABASE_URL"),
-    api_url=os.environ.get("RENDER_EXTERNAL_URL"),
+    api_url=f"{os.environ.get('RENDER_EXTERNAL_URL', '').replace('https://', 'wss://')}:8000/_event",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
