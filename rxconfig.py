a53595c92a8fcef84f20bc698d1a6a567d37e155
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 17:35:21 -0400

    Fix: Configure db_url and api_url for Render

--- rxconfig.py
+++ rxconfig.py
@@ -1,12 +1,12 @@
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="hola",
-    db_url="postgresql://postgres:V32273930@localhost:5432/mi_pagina",
-    api_url="https://YOUR_BACKEND_URL:8000",
+    db_url=os.environ.get("DATABASE_URL", "postgresql://postgres:V32273930@localhost:5432/mi_pagina"),
+    api_url=os.environ.get("RENDER_EXTERNAL_URL"),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
     ]
-
 )
