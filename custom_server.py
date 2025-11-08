a78d8a1c27ffd7749f45b3930e11000408e52315
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-08 09:34:02 -0400

    Actualizar configuración de Reflex y añadir nueva característica

--- custom_server.py
+++ custom_server.py
@@ -1,13 +1,27 @@
+from fastapi import FastAPI, Request
+from backend_api import save_usuario, create_db_and_tables
+from pydantic import BaseModel
 
-import reflex as rx
-from hola.hola import app as reflex_app
-from fastapi import FastAPI
+# Define un modelo Pydantic para los datos del formulario
+class ContactForm(BaseModel):
+    nombre: str
+    correo: str
+    mensaje: str
 
-# This gets the underlying FastAPI app that serves both the backend and the frontend.
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/health")
 async def health_check():
     return {"status": "ok"}
 
-app.mount("/", reflex_app.api)
+@app.post("/contact")
+async def contact_submit(form_data: ContactForm):
+    try:
+        save_usuario(form_data.nombre, form_data.correo, form_data.mensaje)
+        return {"message": "Formulario enviado con éxito"}
+    except Exception as e:
+        return {"message": f"Error al guardar el formulario: {str(e)}", "status": "error"}
