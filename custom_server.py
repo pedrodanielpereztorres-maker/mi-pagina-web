2fb94d10fe14898a178f53b32b09d81203b8c0e3
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 22:50:34 -0400

    fix: Corregir error de websocket y actualizar configuración de despliegue

--- custom_server.py
+++ custom_server.py
@@ -3,4 +3,4 @@ import reflex as rx
 from hola.hola import app as reflex_app
 
 # This gets the underlying FastAPI app that serves both the backend and the frontend.
-app = reflex_app._get_app()
+app = reflex_app.api
