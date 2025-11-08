0a219b09f5bb8eb34b4f3ce5bc7982e5d68e8b7b
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 21:36:40 -0400

    fix: Corregir la URL de la API en la configuración de Reflex

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,7 @@ import os
 config = rx.Config(
     app_name="hola",
     db_url=os.environ.get("DATABASE_URL"),
-    api_url="httpshttps://mi-pagina-web-y37z.onrender.com",
+    api_url="https://mi-pagina-web-y37z.onrender.com",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
