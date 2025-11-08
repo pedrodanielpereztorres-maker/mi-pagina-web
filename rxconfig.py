0f3946710c1d90d1f16b535d985de13fb4d15583
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 21:25:40 -0400

    refactor: Remove explicit api_url from config

--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,6 @@ import os
 config = rx.Config(
     app_name="hola",
     db_url=os.environ.get("DATABASE_URL"),
-    api_url=os.environ.get("RENDER_EXTERNAL_URL", "http://localhost:8000"),
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
