745d43185663634f63589a1e8052fe55adcc67fe
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 22:30:21 -0400

    feat: Actualizar configuración y dependencias para despliegue en Render

---
+++ custom_server.py
@@ -0,0 +1,6 @@
+
+import reflex as rx
+from hola.hola import app as reflex_app
+
+# This gets the underlying FastAPI app that serves both the backend and the frontend.
+app = reflex_app._get_app()
--- rxconfig.py
+++ rxconfig.py
@@ -4,7 +4,6 @@ import os
 config = rx.Config(
     app_name="hola",
     db_url=os.environ.get("DATABASE_URL"),
-    api_url="https://mi-pagina-web-y37z.onrender.com/api",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
