d9f9f2380fe38df045045703e2188e60213001d3
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-07 20:07:03 -0400

    Fix: Conectar metadata del modelo a Alembic

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,4 @@
+from hola.hola import Usuarios
 from logging.config import fileConfig
 import os
 
@@ -21,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Usuarios.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
