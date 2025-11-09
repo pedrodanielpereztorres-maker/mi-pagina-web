4692781f6edf58cdf4a3dab5f5ff8943821de069
pedrodanielpereztorres-maker <pedrodanielpereztorres@gmail.com>
2025-11-09 15:35:43 -0400

    refactor: Remove extend_existing=True from Usuarios model

--- hola/models.py
+++ hola/models.py
@@ -2,7 +2,7 @@ import reflex as rx
 import sqlmodel
 from typing import Optional
 
-class Usuarios(rx.Model, table=True, extend_existing=True):
+class Usuarios(rx.Model, table=True):
     id: Optional[int] = sqlmodel.Field(default=None, primary_key=True)
     nombre: str
     correo: str
--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,7 @@ import reflex as rx
 import os
 
 config = rx.Config(
-    app_name="hola",
+    app_name="app",
     db_url=os.environ.get("DATABASE_URL"),
     plugins=[
         rx.plugins.SitemapPlugin(),
